160fe67bbc99ae9d04ff17d5b694c656f1ede9ac
Fleur41 <Muriithikings456@gmail.com>
2024-01-08 10:51:05 +0300

    if cards

--- Black.py
+++ Black.py
@@ -18,3 +18,54 @@ def show_cards(cards, hidden):
         s = s + '\t ________________'
     if hidden:
         s += '\t ________________'
+    print(s)
+
+    s = ''
+    for card in cards:
+        if card.card_face in ['J', 'Q', 'K', 'A']:
+            s = s + '\t|  {}             |'.format(card.card_face)
+        elif card.value == 10:
+            s = s + '\t|  {}            |'.format(card.value)
+        else:
+            s = s + '\t|  {}            |'.format(card.value)
+
+    if hidden:
+        s += '\t|                |'
+    print(s)
+
+    s = ''
+    for card in cards:
+        s = s + '\t|                |'
+    if hidden:
+        s += '\t|      * *       |'
+    print(s)
+
+    s = ''
+    for card in cards:
+        s = s + '\t|                |'
+    if hidden:
+        s += '\t|    *     *     |'
+    print(s)
+
+    s = ''
+    for card in cards:
+        s = s + '\t|                |'
+    if hidden:
+        s += '\t|   *       *    |'
+    print(s)
+
+    s = ''
+    for card in cards:
+       s = s + '\t|                |'
+    if hidden:
+       s += '\t|   *       *    |'
+    print(s)
+
+    s = ''
+    for card in cards:
+       s = s + '\t|       {}        |'.format(card.symbol)
+    if hidden:
+       s += '\t|          *     |'
+    print(s)
+
+
